27a3b8cc4ab76e17caa4c83f84fb280533f25f9c
Hakim <fh_hamidet@esi.dz>
2021-11-29 22:35:28 +0100

    remove aws deploy support

--- api/apps.py
+++ api/apps.py
@@ -7,9 +7,11 @@ from config.settings.base import ROOT_DIR
 
 
 class ApiConfig(AppConfig):
-    name = 'api'
-    dataplant = pd.read_excel(os.path.join(ROOT_DIR, "data/egrid.xlsx"), sheet_name="PLNT19",
-                         header=1).fillna('')
+    name = "api"
+    dataplant = pd.read_excel(
+        os.path.join(ROOT_DIR, "data/egrid.xlsx"), sheet_name="PLNT19", header=1
+    ).fillna("")
 
-    datastate = data = pd.read_excel(os.path.join(ROOT_DIR, "data/egrid.xlsx"), sheet_name="ST19",
-                             header=1)
+    datastate = data = pd.read_excel(
+        os.path.join(ROOT_DIR, "data/egrid.xlsx"), sheet_name="ST19", header=1
+    )
